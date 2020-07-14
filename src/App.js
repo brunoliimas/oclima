67709314eca75fec188cba3929f8d43c4be44559
Bruno Lima <ibrunoliimas@gmail.com>
2020-07-14 13:47:26 -0300

    iniciando a criação da aplicação

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import axios from 'axios';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h3>Clima nas suas coordenadas (Exemplo)        
+      </h3>
+      <hr/>
+      <ul>
+        <li>Temperatura Atual: xº</li>
+        <li>Temperatura máxima: xº</li>
+        <li>Temperatura minima: xº</li>
+        <li>Pressão: x hpa</li>
+        <li>Umidade: x%</li>
+      </ul>
+    </Fragment>
   );
 }
 
